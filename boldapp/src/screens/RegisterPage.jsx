b6d5d9420b9e645c36744377cb9ddf2c495e369f
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-07-08 23:57:10 +0200

    alignment

--- boldapp/src/screens/RegisterPage.jsx
+++ boldapp/src/screens/RegisterPage.jsx
@@ -4,7 +4,7 @@ import RegistrationForm from '../components/registrationForm';
 
 const RegisterPage = () => {
     return (
-        < section className='rounded-lg bg-lightMint flex lg:flex-row items-center justify-center mx-auto mb-10 w-[90%] p-8' >
+        < section className='rounded-lg bg-lightMint flex lg:flex-row items-center justify-center mx-auto mb-11 w-[90%] p-8' >
             <RegistrationForm />
         </section>
     )
