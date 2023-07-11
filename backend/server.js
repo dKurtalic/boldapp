57768695058266a789e5e24d3366815900f26eff
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-07-12 00:11:40 +0200

    server changes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const path = require('path')
 require('dotenv').config()
-
+const PORT = process.env.PORT || 4000
 
 const app = express()
 
 app.use(express.json());
+app.use(cors())
 app.use('/', routes);
 
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true }).then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log("Connected to database and app successfully running on port 4000")
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true });
+        console.log(`MongoDB Connected`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
     })
-}).catch((error) => {
-    console.log(error)
+})
+app.all('*', (req, res) => {
+    res.json({ "every thing": "is awesome" })
 })
 
 
